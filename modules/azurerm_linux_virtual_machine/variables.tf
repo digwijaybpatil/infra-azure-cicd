44e6cbac32c4a93060464fa7684c61fb8f4ecdda
digwijaybpatil <digwijaybpatil@gmail.com>
2025-11-12 18:04:53 +0530

    created key vault secrets for vm and pushing to kv

---
+++ modules/azurerm_linux_virtual_machine/variables.tf
@@ -0,0 +1,55 @@
+variable "vm_name" {
+  type = string
+  description = "This is the name of the vm"
+}
+
+variable "resource_group_name" {
+  type = string
+  description = "This is the name of the resource group where vm will be created"
+}
+
+variable "location" {
+  type =string
+  description = "The location of the vm"
+}
+
+variable "vm_size" {
+  type = string
+  description = "The size of the vm"
+  default = "Standard_B1s"
+}
+
+variable "admin_username" {
+  type = string
+  description = "admin username for the vm"
+}
+
+variable "network_interface_id" {
+  type = string
+  description = "network inteface id which will be assigned to vm"
+}
+
+variable "ssh_public_key" {
+  type = string
+  description = "This is the public key which will be stored at vm"
+}
+
+variable "caching" {
+  type = string
+  default = "ReadWrite"
+}
+
+variable "storage_account_type" {
+  type = string
+  default = "Standard_LRS"
+}
+
+variable "source_image_reference" {
+  description = "Map defining the source image for the Linux VM."
+  type = object({
+    publisher = string
+    offer     = string
+    sku       = string
+    version   = string
+  })
+}
