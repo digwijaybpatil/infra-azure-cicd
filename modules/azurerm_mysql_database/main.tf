69159023a7152123aac5c5570f93c1c8b5cdd819
digwijaybpatil <digwijaybpatil@gmail.com>
2025-11-20 16:38:31 +0530

    Created bastion host

--- modules/azurerm_mysql_database/main.tf
+++ modules/azurerm_mysql_database/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_mysql_flexible_database" "this" {
   name                = var.database_name
   server_name         = var.server_name
   resource_group_name = var.resource_group_name
-  charset             = "utf8"
-  collation           = "utf8_general_ci"
+  charset             = "utf8mb3"
+  collation           = "utf8mb3_general_ci"
 }
